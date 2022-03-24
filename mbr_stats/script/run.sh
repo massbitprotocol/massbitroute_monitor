2d2e8b02d1dad33ef270dd95f7708b0cf337867e
anhhuy <anhhuy0501@gmail.com>
2022-03-24 11:13:39 +0700

    add deploy script for stats

---
+++ mbr_stats/script/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /opt/stats
+RUST_LOG=info RUST_LOG_TYPE=file
+./mbr-stats update-stats --prometheus-gateway-url https://stat.mbr.massbitroute.dev/__internal_prometheus_gw --mvp-url wss://dev.verification.massbit.io:443 --signer-phrase "bottom drive obey lake curtain smoke basket hold race lonely fit walk" --list-project-url https://portal.massbitroute.dev/mbr/d-apis/project/list/verify
+

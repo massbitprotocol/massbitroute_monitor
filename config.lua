71bdc86a7dde082f4771b76809312d049a36746e
Vu Tran <baysao@gmail.com>
2022-04-05 14:48:45 +0700

    Fix monitor_client path

--- config.lua
+++ config.lua
@@ -8,14 +8,15 @@ local _config = {
     },
     templates = {},
     apps = {},
-    supervisor1 = [[
-
-[program:monitor_refresh]
-command=/bin/bash _SITE_ROOT_/scripts/run loop _refresh_monitors
+    supervisors = {
+        ["monitor_client"] = [[
+[program:monitor_client]
+command=/bin/bash _SITE_ROOT_/../mkagent/agents/push.sh _SITE_ROOT_
 autorestart=true
 redirect_stderr=true
-stdout_logfile=_SITE_ROOT_/logs/monitor_refresh.log
-]],
+stdout_logfile=_SITE_ROOT_/logs/monitor_client.log
+    ]]
+    },
     supervisor = [[
 [program:check_mk_dapi_collect]
 command=/bin/bash _SITE_ROOT_/scripts/checkmk/dapi loop _collect
@@ -23,18 +24,11 @@ autorestart=true
 redirect_stderr=true
 stdout_logfile=_SITE_ROOT_/logs/check_mk_dapi_collect.log
 
-[program:monitor_client]
-command=/bin/bash _SITE_ROOT_/etc/mkagent/agents/push.sh _SITE_ROOT_
-autorestart=true
-redirect_stderr=true
-stdout_logfile=_SITE_ROOT_/logs/monitor_client.log
-
 [program:monitor_server]
 command=/bin/bash _SITE_ROOT_/scripts/server.sh
 autorestart=true
 redirect_stderr=true
 stdout_logfile=_SITE_ROOT_/logs/monitor_server.log
-
     ]]
 }
 return _config

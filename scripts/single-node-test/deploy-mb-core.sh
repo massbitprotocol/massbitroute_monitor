993ab0f277a0ae0d0d07e664eab2b3c6b76150dc
hoanito <hoang@codelight.co>
2022-04-26 17:16:49 +0700

    Test run CI

--- scripts/single-node-test/deploy-mb-core.sh
+++ scripts/single-node-test/deploy-mb-core.sh
@@ -49,7 +49,7 @@ cat massbitroute-core-template-single | \
     sed "s/\[\[PRIVATE_GIT_DOMAIN\]\]/$PRIVATE_GIT_DOMAIN/g"  | \
     sed "s/\[\[PRIVATE_GIT_SSH_USERNAME\]\]/$PRIVATE_GIT_SSH_USERNAME/g" | \
     sed "s/\[\[PRIVATE_GIT_SSH_PASSWORD\]\]/$PRIVATE_GIT_SSH_PASSWORD/g" | \
-    sed "s|\[\[DEPLOY_BRANCH\]\]|$DEPLOY_BRANCH|g" | \
+    sed "s|\[\[DEPLOY_BRANCH\]\]|dev|g" | \
     sed "s/\[\[GIT_API_TOKEN\]\]/$GIT_API_TOKEN/g"  >> test-nodes.tf
 
 ## PORTAL NODE
@@ -64,6 +64,7 @@ cat massbitroute-portal-template-single | \
     sed "s/\[\[PRIVATE_GIT_SSL_PASSWORD\]\]/$PRIVATE_GIT_SSL_PASSWORD/g"  | \
     sed "s/\[\[PRIVATE_GIT_SSH_USERNAME\]\]/$PRIVATE_GIT_SSH_USERNAME/g" | \
     sed "s/\[\[PRIVATE_GIT_SSH_PASSWORD\]\]/$PRIVATE_GIT_SSH_PASSWORD/g" | \
+    sed "s|\[\[DEPLOY_BRANCH\]\]|$DEPLOY_BRANCH|g" | \
     sed "s/\[\[GIT_API_TOKEN\]\]/$GIT_API_TOKEN/g">> test-nodes.tf
 
 # RUST NODE

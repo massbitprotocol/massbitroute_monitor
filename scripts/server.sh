33f0962fff6d9969892bf3dd4d495c21fbed1782
Vu Tran <baysao@gmail.com>
2022-05-11 14:57:38 +0700

    Update script server monitor data

--- scripts/server.sh
+++ scripts/server.sh
@@ -8,28 +8,28 @@ cd $dir
 export TOKEN_FILE=$dir/tokens${name}.txt
 if [ ! -f "$TOKEN_FILE" ]; then touch $TOKEN_FILE; fi
 
-_add() {
-	TYPE=$1
-	ID=$2
-	host=${TYPE}-${ID}
-	if [ -z "$host" ]; then
-		exit 0
-	fi
-	export TOKEN=$(echo -n ${host} | sha1sum | cut -d' ' -f1)
-	if [ ! -f "$TOKEN_FILE" ]; then touch $TOKEN_FILE; fi
-	grep $TOKEN $TOKEN_FILE
-	if [ $? -ne 0 ]; then
-		echo $TOKEN ${host} >>$TOKEN_FILE
-	fi
+# _add() {
+# 	TYPE=$1
+# 	ID=$2
+# 	host=${TYPE}-${ID}
+# 	if [ -z "$host" ]; then
+# 		exit 0
+# 	fi
+# 	export TOKEN=$(echo -n ${host} | sha1sum | cut -d' ' -f1)
+# 	if [ ! -f "$TOKEN_FILE" ]; then touch $TOKEN_FILE; fi
+# 	grep $TOKEN $TOKEN_FILE
+# 	if [ $? -ne 0 ]; then
+# 		echo $TOKEN ${host} >>$TOKEN_FILE
+# 	fi
 
-}
-_kill() {
-	pkill -f server${name}.py
-}
-if [ $# -eq 0 ]; then
-	# $pip --upgrade pip
-	# $pip -r requirements.txt
-	python3 $dir/server${name}.py
-else
-	$@
-fi
+# }
+# _kill() {
+# 	pkill -f server${name}.py
+# }
+# if [ $# -eq 0 ]; then
+# $pip --upgrade pip
+# $pip -r requirements.txt
+python3 $dir/server${name}.py
+# else
+# 	$@
+# fi

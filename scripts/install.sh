fc4e9aef1a8a4284b81b4ab212948b9c7dbf85f9
Vu Tran <baysao@gmail.com>
2022-05-14 16:41:03 +0700

    Update script install check_mk

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 dir=/massbit/massbitroute/app/src/sites/services/monitor
 cd $dir
-curl -kL https://download.checkmk.com/checkmk/2.0.0p17/check-mk-raw-2.0.0p17_0.focal_amd64.deb -o check-mk-raw-2.0.0p17_0.focal_amd64.deb
-dpkg -i check-mk-raw-2.0.0p17_0.focal_amd64.deb
-apt-get install -f
-omd create mbr
+_install() {
+	curl -kL https://download.checkmk.com/checkmk/2.0.0p17/check-mk-raw-2.0.0p17_0.focal_amd64.deb -o check-mk-raw-2.0.0p17_0.focal_amd64.deb
+	dpkg -i check-mk-raw-2.0.0p17_0.focal_amd64.deb
+	apt-get install -f
+	sed 's/Listen 80/Listen 8000/' -i /etc/apache2/ports.conf
+	/etc/init.d/apache2 restart
+	rsync -avz etc/check_mk/* /opt/omd/versions/2.0.0p17.cre/lib/python3/cmk/
+}
 
-# mkdir -p etc/mkagent
-# git clone http://mbr_gateway:6a796299bb72357770735a79019612af228586e7@git.massbitroute.com/massbitroute/mkagent.git  etc/mkagent
-# ln -sf /massbit/massbitroute/app/src/sites/services/monitor/etc/mkagent/agents/main.mk /opt/omd/sites/mbr/etc/check_mk/main.mk
-omd start mbr
-sed 's/Listen 80/Listen 8000/' -i /etc/apache2/ports.conf
-/etc/init.d/apache2 restart
-rsync -avz etc/check_mk/* /opt/omd/versions/2.0.0p17.cre/lib/python3/cmk/
-su - mbr -c 'htpasswd etc/htpasswd cmkadmin'
+_add() {
+	_name=$1
+	omd create _name
+
+	# mkdir -p etc/mkagent
+	# git clone http://mbr_gateway:6a796299bb72357770735a79019612af228586e7@git.massbitroute.com/massbitroute/mkagent.git  etc/mkagent
+	# ln -sf /massbit/massbitroute/app/src/sites/services/monitor/etc/mkagent/agents/main.mk /opt/omd/sites/mbr/etc/check_mk/main.mk
+	omd start $_name
+	su - mbr -c 'htpasswd etc/htpasswd cmkadmin'
+}
+$@

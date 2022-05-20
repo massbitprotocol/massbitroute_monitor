3f7a5e3cf0babe98caf5269ba03b433d3649420b
Vu Tran <baysao@gmail.com>
2022-05-20 15:31:45 +0700

    Add node check

--- src/check_port.lua
+++ src/check_port.lua
@@ -1,1 +1,18 @@
-ngx.say("Check port")
+local shell = require "shell-games"
+local json = require "cjson"
+local _ip = ngx.var.ip
+local _proto = ngx.var.proto
+local _port = ngx.var.port
+local _opt = "-vz"
+if _proto == "udp" then
+    _opt = "-uvz"
+end
+
+local _cmd = {
+    "/usr/bin/nc",
+    _opt,
+    _ip,
+    _port
+}
+local _res = shell.run(_cmd)
+return json.encode(_res)

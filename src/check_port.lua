9328252381b5b423fcc8d430310092fd37b5a917
Vu Tran <baysao@gmail.com>
2022-05-20 15:36:24 +0700

    Add node check

--- src/check_port.lua
+++ src/check_port.lua
@@ -3,6 +3,9 @@ local json = require "cjson"
 local _ip = ngx.var.ip
 local _proto = ngx.var.proto
 local _port = ngx.var.port
+ngx.log(ngx.ERR, "ip:" .. _ip)
+ngx.log(ngx.ERR, "port:" .. _port)
+ngx.log(ngx.ERR, "proto:" .. _proto)
 local _opt = "-vz"
 if _proto == "udp" then
     _opt = "-uvz"

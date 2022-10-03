e9e51ef060a30671737161a9bab49def1fcda58f
Vu Tran <baysao@users.noreply.github.com>
2022-10-03 17:19:22 +0700

    Refactor monitor (#90)
    * refactor script run
    
    * remove NSTALE force
    
    * fix: remove dep on https
    
    * fix: replace _loop
    
    * fix(monitor_server): update token path
    
    * fix(monitor_server): update token path
    
    * chore: add check_mk deb
    
    * chore: update install with combine multi file from source
    
    * chore: add apt update before install
    
    * chore: add check_mk installation to prepare stage
    
    * fix: update wrong path
    
    * chore: remove hosts modify
    
    * fix: make directory before create
    
    * chore: update env to vars file
    
    * chore: update env to vars file
    
    * chore: fix syntax
    
    * chore: fix syntax
    
    * fix: add create dir
    
    * fix: update loop timeout on config
    
    * fix: update curl option

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,8 +2,10 @@
 dir=/massbit/massbitroute/app/src/sites/services/monitor
 cd $dir
 _install() {
-	curl -kL -C - https://download.checkmk.com/checkmk/2.0.0p17/check-mk-raw-2.0.0p17_0.focal_amd64.deb -o check-mk-raw-2.0.0p17_0.focal_amd64.deb
-	dpkg -i check-mk-raw-2.0.0p17_0.focal_amd64.deb
+	#	curl -kL -C - https://download.checkmk.com/checkmk/2.0.0p17/check-mk-raw-2.0.0p17_0.focal_amd64.deb -o check-mk-raw-2.0.0p17_0.focal_amd64.deb
+	cat scripts/checkmk/checkmka* >/tmp/check-mk-raw-2.0.0p17_0.focal_amd64.deb
+	apt-get update
+	dpkg -i /tmp/check-mk-raw-2.0.0p17_0.focal_amd64.deb
 	apt-get -f install -y
 	sed 's/Listen 80/Listen 8000/' -i /etc/apache2/ports.conf
 	/etc/init.d/apache2 restart
@@ -19,5 +21,9 @@ _add() {
 	# ln -sf /massbit/massbitroute/app/src/sites/services/monitor/etc/mkagent/agents/main.mk /opt/omd/sites/mbr/etc/check_mk/main.mk
 	omd start $_name
 	su - $_name -c "htpasswd /opt/omd/sites/$_name/etc/htpasswd cmkadmin"
+}
+_init() {
+	_install
+	_add mbr
 }
 $@

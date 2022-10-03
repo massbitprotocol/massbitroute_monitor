af51e0de4ef5883b96c18234e340d423a25c2fe3
Vu Tran <baysao@gmail.com>
2022-10-03 15:43:43 +0700

    chore: update install with combine multi file from source

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,8 +2,9 @@
 dir=/massbit/massbitroute/app/src/sites/services/monitor
 cd $dir
 _install() {
-	curl -kL -C - https://download.checkmk.com/checkmk/2.0.0p17/check-mk-raw-2.0.0p17_0.focal_amd64.deb -o check-mk-raw-2.0.0p17_0.focal_amd64.deb
-	dpkg -i check-mk-raw-2.0.0p17_0.focal_amd64.deb
+	#	curl -kL -C - https://download.checkmk.com/checkmk/2.0.0p17/check-mk-raw-2.0.0p17_0.focal_amd64.deb -o check-mk-raw-2.0.0p17_0.focal_amd64.deb
+	cat scripts/checkmk/checkmka* >/tmp/check-mk-raw-2.0.0p17_0.focal_amd64.deb
+	dpkg -i /tmp/check-mk-raw-2.0.0p17_0.focal_amd64.deb
 	apt-get -f install -y
 	sed 's/Listen 80/Listen 8000/' -i /etc/apache2/ports.conf
 	/etc/init.d/apache2 restart

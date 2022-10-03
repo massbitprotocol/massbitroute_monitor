114d210eeb008368fdb1470d587408ebc7e32bd6
Vu Tran <baysao@gmail.com>
2022-10-03 15:44:49 +0700

    chore: add apt update before install

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,6 +4,7 @@ cd $dir
 _install() {
 	#	curl -kL -C - https://download.checkmk.com/checkmk/2.0.0p17/check-mk-raw-2.0.0p17_0.focal_amd64.deb -o check-mk-raw-2.0.0p17_0.focal_amd64.deb
 	cat scripts/checkmk/checkmka* >/tmp/check-mk-raw-2.0.0p17_0.focal_amd64.deb
+	apt-get update
 	dpkg -i /tmp/check-mk-raw-2.0.0p17_0.focal_amd64.deb
 	apt-get -f install -y
 	sed 's/Listen 80/Listen 8000/' -i /etc/apache2/ports.conf
